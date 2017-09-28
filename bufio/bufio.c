1bf3324d03c77f12ec9e79ea94570b4eb06b080c
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2017-09-28 13:39:43 +0200

    started small bufio c project

---
+++ bufio/bufio.c
@@ -0,0 +1,6 @@
+#include "bufio.h"
+
+char *bufread(FILE *in, int n)
+{
+    return NULL;
+}
---
+++ bufio/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "bufio.h"
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    return 0;
+}
