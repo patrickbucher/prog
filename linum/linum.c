7310f51026a95f7f3b07287206b184ad8884e9da
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-08-15 12:54:17 +0200

    linum: buffer input for prior line counting

--- linum/linum.c
+++ linum/linum.c
@@ -7,29 +7,55 @@
 
 char *numfmt(unsigned int);
 unsigned int npos(unsigned int);
+void alloc_fail(int);
 
 int main(int argc, char *argv[])
 {
-	int c, line;
+	const int bufalloc = 1024;
+
+	int *buf, bufp, c, nlines, line, bufsize, allocbytes, i;
 	bool print_line;
 	char *fmt;
 
-	// TODO: read whole file in buffer, count lines, then process buffer
-	fmt = numfmt(99);
+	nlines = 0;
+	allocbytes = sizeof(int) * bufalloc;
+	buf = (int *)malloc(allocbytes);
+	if (buf == NULL) {
+		alloc_fail(allocbytes);
+	}
+	bufsize = bufalloc;
+	for (bufp = 0; (c = getchar()) != EOF; bufp++) {
+		if (bufp + 1 >= bufsize) {
+			allocbytes = sizeof(int) * (bufsize + bufalloc);
+			buf = realloc(buf, allocbytes);
+			if (buf == NULL) {
+				alloc_fail(allocbytes);
+			}
+			bufsize += bufalloc;
+		}
+		if (c == '\n') {
+			nlines++;
+		}
+		buf[bufp] = c;
+	}
+	buf[bufp] = '\0';
 
+	fmt = numfmt(nlines);
 	line = 1;
 	print_line = true;
-	while ((c = getchar()) != EOF) {
+	for (i = 0; i < bufp; i++) {
 		if (print_line) {
 			printf(fmt, line);
 			print_line = false;
 		}
-		if (c == '\n') {
+		if (buf[i] == '\n') {
 			print_line = true;
 			line++;
 		}
-		putchar(c);
+		putchar(buf[i]);
 	}
+
+	free(buf);
 	free(fmt);
 
 	return 0;
@@ -50,8 +76,7 @@ char *numfmt(unsigned int nlines) {
 
 	fmt = (char *)malloc(sizeof(char) * nbytes);
 	if (fmt == NULL) {
-		fprintf(stderr, "unable to allocate %d bytes\n", nbytes);
-		exit(EXIT_FAILURE);
+		alloc_fail(nbytes);
 	}
 	snprintf(fmt, nbytes, "%%%dd ", cols);
 
@@ -69,4 +94,11 @@ unsigned int npos(unsigned int n) {
 	} while (n > 0);
 
 	return pos;
+}
+
+// alloc_fail prints an error message for nbytes that could not be allocated
+// and exits the program.
+void alloc_fail(int nbytes) {
+	fprintf(stderr, "unable to allocate %d bytes\n", nbytes);
+	exit(EXIT_FAILURE);
 }
